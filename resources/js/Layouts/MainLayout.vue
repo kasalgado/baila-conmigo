beed7bd513e00af41b99de7c86556b238379d276
Kleber Salgado <kas_ecu@yahoo.com>
2024-03-05 10:47:17 +0100

    Inertia Layout added

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <Link href="/">Home</Link> | <Link href="/profile">Profile</Link>
+    <div>Logged time: {{ timer }}</div>
+    <slot></slot>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+const timer = ref(0)
+setInterval(() => timer.value++, 1000)
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [IndexController::class, 'index']);
+Route::get('/profile', [ProfileController::class, 'index']);
